a478ac86e320cbe5e73b011f53db4c204a476d2c
CVdaZN <caio_vinicius2246@hotmail.com>
2022-10-24 09:53:51 -0300

    estilização da pagina

---
+++ src/test/java/ww/Mara_Afro/MaraAfroApplicationTests.java
@@ -0,0 +1,13 @@
+package ww.Mara_Afro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaraAfroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
